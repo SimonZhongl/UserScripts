1fc43c863ccdcddc68c9e9a498b8d1d62bc6b505
SimonZhongl <SimonZhongl@users.noreply.github.com>
2023-04-10 13:16:56 +0800

    Update Display_anti-hotlinking_images_For_Inoreader.user.js
    modify homepage

--- Display_anti-hotlinking_images_For_Inoreader.user.js
+++ Display_anti-hotlinking_images_For_Inoreader.user.js
@@ -2,7 +2,7 @@
 // @name             显示防盗链图片 for Feedly
 // @name:en          Display anti-hotlinking images For Feedly
 // @version          0.1
-// @namespace        https://github.com/maboloshi/UserScripts/
+// @namespace        https://github.com/SimonZhongl/UserScripts
 // @description      通过强制全站不发送 referrer， 实现显示防盗链图片
 // @description:en   Display anti-hotlinking images, for forced not send a referrer.
 // @match          http*://feedly.com/*
